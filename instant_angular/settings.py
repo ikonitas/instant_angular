f9d68fa615b99015c7768518115f3f2541780b2d
Edvinas Jurevicius <ikonitas@gmail.com>
2014-07-31 21:09:43 +0100

    angularJs hooked in django.

--- instant_angular/settings.py
+++ instant_angular/settings.py
@@ -85,8 +85,8 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = (
+    os.path.join(BASE_DIR, 'static')
 )
 
 STATICFILES_DIRS = (
